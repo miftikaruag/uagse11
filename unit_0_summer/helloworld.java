3092d81e1bf82a1289753f9f0fcfd63da3f833f2
miftikar <muhammediftikar@uagateway.org>
2016-10-24 18:11:05 +0000

    initial commit

---
+++ unit_0_summer/helloworld.java
@@ -0,0 +1,7 @@
+public class helloworld{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.println("Welcome to APCSA!");
+        System.out.println("I wanna try pokemon go but it doesn't work in Bangladesh!! :'( ");
+    }
+}
